5a04d1deb239434c66288bfd6c701c64ba8d8d8d
myasinK <yasinul@gmail.com>
2018-09-20 23:50:52 -0700

    Kata-DuplicateEncoder

---
+++ Kata-DuplicateEncoder/Kata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Kata_DuplicateEncoder
+{
+    public class Kata
+    {
+        public static string DuplicateEncode(string word)
+        {
+            var sb = new StringBuilder(word);
+            var count = 0;
+            foreach (var c in word)
+            {
+                sb[count] = IsMultiple(c, word) && (c != '(' || c != ')') ? ')' : '(';
+                count++;
+            }
+
+            return sb.ToString();
+        }
+
+        public static bool IsMultiple(char c, string word)
+        {
+            var last = word.LastIndexOf(c.ToString(), StringComparison.CurrentCultureIgnoreCase);
+            var first = word.IndexOf(c.ToString(),StringComparison.CurrentCultureIgnoreCase);
+            return (last != first);
+        }
+    }
+}
---
+++ Kata-DuplicateEncoder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//    The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+//Examples:
+
+//"din" => "((("
+
+//"recede" => "()()()"
+
+//"Success" => ")())())"
+
+//"(( @" => "))(("
+
+namespace Kata_DuplicateEncoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newStr = Kata.DuplicateEncode("Success");
+            Console.WriteLine(newStr);
+            Console.WriteLine(Kata.IsMultiple('s',"Sus"));
+
+        }
+    }
+}
